d48299c00114c785a6735fec606a0127a50a97da
velocityCalvary <xyu530@uw.edu>
2021-05-13 21:04:52 -0700

    edit stop  card

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from card import UnoCard
 
 
 class UnoGame:
-    def __init__(self, players, time_number_cards=2, time_num_function=1):
+    def __init__(self, players=2, time_number_cards=2, time_num_function=1):
 
         # initialize players
 
@@ -17,31 +17,37 @@ class UnoGame:
 
         colors = list(range(4))  # 0, 1, 2, 3
         numbers = list(range(1, 10)) # 1, 2, 3..., 9
-        functions = ["plus2", "plus4", "stop", "change color"]
+        functions = ["plus4", "change color"]
 
         deck = []
 
         # create number cards:
-        for _ in time_number_cards:
+        for _ in range(time_number_cards):
             for color in colors:
                 for number in numbers:
                     card = UnoCard(color=color, number=number, type=None)
                     deck.append(card)
 
-        # create plus2 cards:
-        for _ in time_number_cards:
+        # create plus2 and stop cards:
+        for _ in range(time_number_cards):
             for color in colors:
-                card = UnoCard(color=color, number=None, type="plus2")
-                deck.append(card)
+                for function in ['plus 2', 'stop']:
+                    card = UnoCard(color=color, number=None, type=function)
+                    deck.append(card)
 
         # create other functional cards:
-        for _ in time_num_function:
-            for function in functions[1:]:
-                for _ in range(4):
+        for _ in range(time_num_function):
+            for function in ["plus4", "change color"]:
+                for i in range(4):
                     card = UnoCard(color=None, number=None, type=function)
                     deck.append(card)
 
         return deck
+
+
+    def get_deck_str(self):
+        return str([str(x) for x in self.deck])
+
 
 
 
